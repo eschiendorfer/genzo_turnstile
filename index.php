269be5f20940bfdb15b42b197fc63bfa5fc014a8
Emanuel Schiendorfer <mandi.schiendorfer@gmail.com>
2023-01-16 15:44:10 +0100

    Update copyright headers

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Copyright (C) 2018 Emanuel Schiendorfer
+ * Copyright (C) 2023 Emanuel Schiendorfer
  *
  * @author    Emanuel Schiendorfer <https://github.com/eschiendorfer>
- * @copyright 2018 Emanuel Schiendorfer
+ * @copyright 2023 Emanuel Schiendorfer
  * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 				    	
